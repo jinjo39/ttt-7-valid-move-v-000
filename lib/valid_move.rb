2ddd7e8ae72b294b9b8c73bb7e20c9f40e9c00ed
Christina Delavar <c.delavar@gmail.com>
2018-08-15 20:51:14 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 def valid_move?(board, index)
-  board[index] = board[index].to_i
-  if position_taken?(board,index) == false && board[index].between(0, 8)
+  index = index.to_i
+  if position_taken?(board,index) == false && index.between(0, 8)
     return true
   else
     return false
